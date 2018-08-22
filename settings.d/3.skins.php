cbed24ba5c50dbe0dc0cefbc907c2c1d3a1f79c6
Jan Drewniak <jan.drewniak@gmail.com>
2018-08-22 20:28:51 +0200

    updating MF and minerva configs

--- settings.d/3.skins.php
+++ settings.d/3.skins.php
@@ -5,3 +5,9 @@ wfLoadSkin( 'Vector' );
 wfLoadSkin( 'MinervaNeue' );
 $wgDefaultSkin = "vector";
 # $wgDefaultSkin = 'minerva';
+
+// -- Minerva Neue template hacks -- //
+$wgMinervaApplyKnownTemplateHacks = 1;
+$wgMinervaPageIssuesTest = true;
+$wgMinervaABSamplingRate = 1;
+
--- settings.d/4.mobilefrontend.php
+++ settings.d/4.mobilefrontend.php
@@ -3,3 +3,15 @@
 wfLoadExtension( 'MobileFrontend' );
 $wgMFAutodetectMobileView = true;
 $wgMFDefaultSkinClass = 'SkinMinerva';
+
+// -- Mobile frontend content --//
+$wgMFNearbyEndpoint = 'https://en.m.wikipedia.org/w/api.php';
+$wgMFContentProviderClass = 'MobileFrontend\ContentProviders\MwApiContentProvider';
+$wgMFMwApiContentProviderBaseUri = 'https://en.m.wikipedia.org/w/api.php';
+$wgMFDisplayWikibaseDescriptions = [
+    'search' => true,
+    'nearby' => true,
+    'watchlist' => true,
+    'tagline' => true,
+];
+
